4700683d75f4a9f453ccb547f1c860e7314fd972
Lucas Ogden-Davis <lucas@ogdendavis.com>
2018-12-17 13:45:56 -0500

    Add content containers in index.php

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php get_header(); ?>
-  <h1>Ask Ms. Lord!</h1>
-  <p>A place for students and parents to get information and guidance on the
-    college admission process from an experienced professional</p>
+
+  <div id = "primary" class = "content-area">
+
+    <main id = "main" class = "site-main" role = "main">
+
+      <!-- This is where the loop will start -->
+      <article id = "post-<?php the_ID(); ?>" <?php post_class(); ?> >
+
+        <header class = "entry-header">
+          <h1>Ask Ms. Lord!</h1>
+        </header>
+
+        <div class = "entry-content">
+          <p>A place for students and parents to get information and guidance on the college admission process from an experienced professional</p>
+        </div>
+
+      </article>
+      
+    </main>
+
+  </div>
+
 <?php get_footer(); ?>
