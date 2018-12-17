346f6cbdb42ee574204613d90e858c25c6f142bc
Lucas Ogden-Davis <lucas@ogdendavis.com>
2018-12-17 12:17:54 -0500

    Add header.php and footer.php

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Ask Ms. Lord</title>
-  </head>
-  <body>
-    <h1>Ask Ms. Lord!</h1>
-    <p>A place for students and parents to get information and guidance on the
-      college admission process from an experienced professional</p>
-  </body>
-</html>
+<?php get_header(); ?>
+<h1>Ask Ms. Lord!</h1>
+<p>A place for students and parents to get information and guidance on the
+  college admission process from an experienced professional</p>
+<?php get_footer(); ?>
