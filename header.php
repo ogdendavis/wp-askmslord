70b2ab068c5b0996950b6eb55770762914d37396
Lucas Ogden-Davis <lucas@ogdendavis.com>
2018-12-17 13:27:54 -0500

    Add accessibility content link and masthead

--- header.php
+++ header.php
@@ -9,13 +9,32 @@
 
     <div id = "page">
 
-      <nav id = "site-navigation" class = "main-navigation" role = "navigation">
-        <?php
-          $args = [
-            'theme_location' => 'main-menu'
-          ];
-          wp_nav_menu( $args );
-        ?>
-      </nav>
+      <a href = "#content" class = "skip-link screen-reader-text">
+        <?php esc_html_e( 'Skip to content', 'askmslord' ); ?>
+      </a>
+
+      <header id = "masthead" class = "site-header" role = "banner">
+
+        <div class = "site-branding">
+          <p class = "site-title">
+            <a href = "<?php echo esc_url( home_url( '/' ) ); ?>" rel = "home">
+              <?php bloginfo( 'name' ); ?>
+            </a>
+          </p>
+          <p class = "site-description">
+            <?php bloginfo( 'description' ); ?>
+          </p>
+        </div>
+
+        <nav id = "site-navigation" class = "main-navigation" role = "navigation">
+          <?php
+            $args = [
+              'theme_location' => 'main-menu'
+            ];
+            wp_nav_menu( $args );
+          ?>
+        </nav>
+
+      </header>
 
       <div id = "content" class = "site-content">
