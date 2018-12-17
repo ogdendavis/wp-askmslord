a4d9d11635c3537e177f34f9d3e19c92ea0679cf
Lucas Ogden-Davis <lucas@ogdendavis.com>
2018-12-17 15:21:51 -0500

    Add photo to sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,7 @@
 <aside id = "secondary" class = "sidebar bio" role = "complementary">
 
+  <img src = "wp-content/uploads/2018/12/ms-lord-320x480-200x300.jpg" class = "sidebar-image">
+
   <p>Ms. Kimberly Lord has been in the college admission and financial aid field since 2006. In addition to working with students at her current school, she shares her knowledge and expertise with students around the world through askmslord.com.</p>
 
 </aside>
