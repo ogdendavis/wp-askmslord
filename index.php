1bd64664601ca2bb8ad72cc26dcddf4762f612a0
Lucas Ogden-Davis <lucas@ogdendavis.com>
2018-12-17 11:39:09 -0500

    Add dummy/starter content & styling

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Ask Ms. Lord</title>
+  </head>
+  <body>
+    <h1>Ask Ms. Lord!</h1>
+    <p>A place for students and parents to get information and guidance on the
+      college admission process from an experienced professional</p>
+  </body>
+</html>
