802f6de170d3a3bda91bb3ead1fe69816a87e32c
Lucas Ogden-Davis <lucas@ogdendavis.com>
2018-12-18 09:16:12 -0500

    Add template-parts

--- index.php
+++ index.php
@@ -5,28 +5,14 @@
     <main id = "main" class = "site-main" role = "main">
 
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <article id = "post-<?php the_ID(); ?>" <?php post_class(); ?> >
 
-          <header class = "entry-header">
-            <?php the_title( '<h1>', '</h1>' ); ?>
-          </header>
+        <?php get_template_part( 'template-parts/content' ); ?>
 
-          <div class = "entry-content">
-            <?php the_content(); ?>
-          </div>
-        </article>
-        <?php endwhile; else: ?>
-          <article id = "post-<?php the_ID(); ?>" <?php post_class(); ?> >
+      <?php endwhile; else: ?>
 
-            <header class = "entry-header">
-              <h1><?php esc_html_e( '404', 'askmslord' ); ?></h1>
-            </header>
-
-            <div class = "entry-content">
-              <p><?php esc_html_e( 'Oops! That page doesn\'t exist.', 'askmslord' ); ?></p>
-            </div>
-          </article>
-        <?php endif; ?>
+          <?php get_template_part( 'template-parts/content', 'none' ); ?>
+          
+      <?php endif; ?>
 
     </main>
 
---
+++ template-parts/content-none.php
@@ -0,0 +1,10 @@
+<article id = "post-<?php the_ID(); ?>" <?php post_class(); ?> >
+
+  <header class = "entry-header">
+    <h1><?php esc_html_e( '404', 'askmslord' ); ?></h1>
+  </header>
+
+  <div class = "entry-content">
+    <p><?php esc_html_e( 'Oops! That page doesn\'t exist.', 'askmslord' ); ?></p>
+  </div>
+</article>
