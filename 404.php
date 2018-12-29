d9d19deff105b4aa2c7a5d46bd79f576bb408747
Lucas Ogden-Davis <lucas@ogdendavis.com>
2018-12-29 11:04:02 -0700

    Add 404.php

---
+++ 404.php
@@ -0,0 +1,15 @@
+<!-- 404.php -->
+
+<?php get_header(); ?>
+
+  <div id = "primary" class = "content-area full-width error">
+
+    <main id = "main" class = "site-main" role = "main">
+
+      <?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+    </main>
+
+  </div>
+
+<?php get_footer(); ?>
--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -3,10 +3,14 @@
 <article id = "post-<?php the_ID(); ?>" <?php post_class(); ?> >
 
   <header class = "entry-header">
-    <h1><?php esc_html_e( '404', 'askmslord' ); ?></h1>
+    <h1><?php esc_html_e( 'Oops! That page doesn\'t exist.', 'askmslord' ); ?></h1>
   </header>
 
   <div class = "entry-content">
-    <p><?php esc_html_e( 'Oops! That page doesn\'t exist.', 'askmslord' ); ?></p>
+
+    <p>Ms. Lord regrets to inform you that this page doesn't exist! Perhaps you'd like to take this opporunity to search her site for some content related to what you were looking for?</p>
+
+    <?php echo get_search_form(); ?>
+
   </div>
 </article>
